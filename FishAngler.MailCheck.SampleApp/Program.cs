4689cdb54dc647b64e3921d68249941dd569d148
Jonas Stawski <jstawski@live.com>
2016-07-20 12:46:59 -0400

    Added sample Console Application

---
+++ FishAngler.MailCheck.SampleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishAngler.MailCheck.SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DefaultRun();
+            CustomRun();
+            Console.ReadLine();
+        }
+
+        static void DefaultRun()
+        {
+            var mailCheck = new MailCheck();
+            var suggestion = mailCheck.Suggest("test@hotmal.con");
+            if (suggestion != null)
+            {
+                Debug.WriteLine($"Suggested email: {suggestion.Full}");
+            }
+            
+        }
+
+        static void CustomRun()
+        {
+            string[] domains = { "custom-domain.com", "hotmail.com", "gmail.com", "yahoo.com" };
+            string[] secondLevelDomains = { "custom-domain", "hotmail", "gmail", "yahoo" };
+            string[] topLevelDomains = { "com" };
+
+            var mailCheck = new MailCheck(domains, secondLevelDomains, topLevelDomains);
+            var suggestion = mailCheck.Suggest("test@hotmal.con");
+            if (suggestion != null)
+            {
+                Debug.WriteLine($"Suggested email: {suggestion.Full}");
+            }
+
+            suggestion = mailCheck.Suggest("test@custom-doman.con");
+            if (suggestion != null)
+            {
+                Debug.WriteLine($"Suggested email: {suggestion.Full}");
+            }
+        }
+    }
+}
